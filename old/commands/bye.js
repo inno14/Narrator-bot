c1dcbaaa8297623d04dce9929a3e27e7efa1fe5f
Sampada Petkar <sampadap03@gmail.com>
2021-03-01 12:53:41 +0530

    Create directory structure for Version 2
    Signed-off-by: Sampada Petkar <sampadap03@gmail.com>
    Signed-off-by: Sampada Petkar <sampadap33@gmail.com>

---
+++ old/commands/bye.js
@@ -0,0 +1,54 @@
+module.exports = {
+    name: "bye",
+    run: async (message, args, client) => {
+        let role = message.guild.roles.cache.find(r => r.name === "Narrator")
+        let role2 = message.guild.roles.cache.find(r => r.name === "Narrator Trainee")
+        if (message.member.roles.cache.has(role.id) || message.member.roles.cache.has(role2.id)) {
+            message.channel.send('bye bye') 
+            setTimeout(function () {
+                for (let i = 1 ; i < 16 ; i++) {
+                    let guy = message.guild.members.cache.find(m => m.nickname === i.toString())
+                    if (guy) {
+                        guy.kick()
+                    }
+                }
+                let spec = message.guild.roles.cache.find(r => r.name === "Spectator")
+                spec.members.forEach(e => {e.kick()})
+            }, 5000)
+            let channels = message.guild.channels.cache.filter(c => c.name.startsWith("priv") && c.parentID != "748959630520090626")
+            channels.forEach(async e => {
+                let msgs = await e.messages.fetch()
+                let total = await msgs.filter(m => !m.pinned && (Date.now() - m.createdTimestamp < (60*60*24*14)))
+                
+                if (total.size > 0) {
+                    e.bulkDelete(total)
+                }
+            })
+            let tempchannels = message.guild.channels.cache.filter(c => c.parentID === "748959630520090626")
+            tempchannels.forEach(e => e.delete())
+            let emsgs = await message.guild.channels.cache.find(c => c.name === "enter-game").messages.fetch()
+            let oki = emsgs.filter(m => !m.pinned && (Date.now() - m.createdTimestamp < (60*60*24*14)))
+            message.guild.channels.cache.find(c => c.name === "enter-game").bulkDelete(oki)
+            let settings = message.guild.channels.cache.filter(c => c.parentID === "606250714355728395")
+            settings.forEach(async e => {
+                let oki = await e.messages.fetch()
+                let hmm = oki.filter(m => !m.pinned && (Date.now() - m.createdTimestamp < (60*60*24*14)))
+                if (hmm.size > 0) {
+                    e.bulkDelete(hmm)
+                }
+            })
+            let chans = ["vote-chat", "music-commands", "shadow-votes", "jailed-chat", "werewolves-chat", "time", "dead-chat", "day-chat"]
+            let ingame = message.guild.channels.cache.filter(c => c.parentID === "606132962752331839" && chans.includes(c))
+            ingame.forEach(async e => {
+                let ashish = await e.messages.fetch()
+                let filt = ashish.filter(c => !c.pinned && (Date.now() - c.createdTimestamp < (60*60*24*14)))
+                if (filt.size < 100) {
+                    e.bulkDelete(filt)
+                } else {
+                    filt = filt.filter(m => m.size < 101  && (Date.now() - m.createdTimestamp < (60*60*24*14)))
+                    e.bulkDelete(filt)
+                }
+            })
+        }
+    }
+}
---
+++ old/commands/setwin.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "setwin",
+    run: async (message, args, client) => {
+        if (message.guild.id == "472261911526768642") {
+            if (message.member.roles.cache.has("606139219395608603") || message.member.roles.cache.has("606276949689499648")) {
+                    require("quick.db").set(`winner`, args.join(' '))
+                    message.channel.send("Done!")
+                    require("quick.db").set(`isDay_${message.guild.id}`, "yes")
+                    require("quick.db").set(`isNight_${message.guild.id}`, "yes")
+                    require("quick.db").set(`commandEnabled_${message.guild.id}`, "yes")
+            }
+        }
+    }
+}
